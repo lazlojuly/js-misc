c0176738f7d1865b1d350221c5b48637adf516f9
lazlojuly <lazlo.gy@gmail.com>
2016-02-15 13:56:27 +0000

    More examples for linked-list length and count

--- linked-list-length-and-count.js
+++ linked-list-length-and-count.js
@@ -46,3 +46,52 @@ function count(head, integer) {
 	return count;
 }
 
+
+
+// best practice as well as most clever
+function Node(data) {
+  this.data = data
+  this.next = null
+}
+
+function length(head) {
+  return head ? 1 + length(head.next) : 0
+}
+
+function count(head, data) {
+  if (!head) return 0
+  return (head.data === data ? 1 : 0) + count(head.next, data)
+}
+
+
+// basic, easy to understand
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}
+
+function length(head) {
+  let length = 0;
+  let current = head;
+
+  while ( current != null ) {
+    current = current.next
+    length++;
+  }
+
+  return length;
+}
+
+function count(head, data) {
+  let current = head;
+  let count = 0;
+
+  while ( current != null ) {
+    if ( current.data === data )
+      count++;
+
+    current = current.next;
+  }
+
+  return count;
+}
