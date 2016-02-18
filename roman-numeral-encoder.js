d65439ebf1ec1dad82a0ce75b658da3a4a35f0f8
lazlojuly <lazlo.gy@gmail.com>
2016-02-18 19:09:45 +0000

    debug roman encoder

--- roman-numeral-encoder.js
+++ roman-numeral-encoder.js
@@ -23,17 +23,36 @@ function solution(n) {
 
   let result = [];
 
-  symbols.forEach(function(symbol) {
+  symbols.forEach(function(symbol, index) {
     let quotient = Math.floor(n/symbol.num);
+    console.log(quotient, 'quotient');
     if (quotient >= 1) {
-      result = result.concat(Array(quotient).fill(symbol.rom));
-      n = n - quotient*symbol.num;
+      if (quotient <= 3) {
+        // look ahead
+        let n2 = n - quotient*symbol.num;
+        let next = symbols[index+1];
+        if (next && (Math.floor(n2/next.num) > 3)) {
+          // more than 3 symbols would be needed for next value
+          result.push(symbols[index+1].rom);
+          result.push(symbols[index-1].rom);
+          n = n - (symbols[index-1].num - symbols[index+1].num);
+        } else {
+          // safe to turn current value to symbol(s)
+          let syms = [];
+          for (let x=1;x<=quotient;x++) {
+            syms.push(symbol.rom);
+          }
+          result = result.concat(syms);
+          n = n - (quotient * symbol.num);
+        }
+      }
     }
   });
 
   return result.join('');
 }
 
-console.log(solution(1990), 'MCMXC');
-console.log(solution(2008), 'MMVIII');
-console.log(solution(1666), 'MDCLXVI');
+console.log(solution(4), 'IV');
+// console.log(solution(1990), 'MCMXC');
+// console.log(solution(2008), 'MMVIII');
+// console.log(solution(1666), 'MDCLXVI');
