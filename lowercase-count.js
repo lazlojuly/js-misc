5ab8219a45d4e75efcd1c9a9cf1ae9c43d39b1ef
lazlojuly <lazlo.gy@gmail.com>
2016-02-18 18:30:57 +0000

    lowercase-count.js

---
+++ lowercase-count.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function lowercaseCount(str){
+  let matches = str.match(/[a-z]/g);
+  return (matches) ? matches.length : 0;
+}
+
+console.log(lowercaseCount("abc"), 3);
+console.log(lowercaseCount("abcABC123"), 3);
