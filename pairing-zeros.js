2eb4ffee6f988ab69d5cc8ea709b003568bbcc26
lazlojuly <lazlo.gy@gmail.com>
2016-02-19 12:22:43 +0000

    updating pair zeros

--- pairing-zeros.js
+++ pairing-zeros.js
@@ -1,13 +1,21 @@
 function pairZeros(arr) {
-
   let pairing = false;
   return arr.reduce((result,cur,index) => {
     if (cur !== 0) {
       result.push(cur);
     } else {
-      if (pairing) { result.push(cur); }
-      pairing = !pairing;
+      if (pairing) {
+        pairing = false;
+      } else {
+        pairing = true;
+        result.push(cur);
+      }
     }
     return result;
   },[]);
+}
+
+// clever
+function pairZeros(a, f) {
+  return a.filter(function(n) { return (n != 0 || (f = !f)); });
 }
